df816a1359613f933265cb2c493d5a6f7fe570d6
KoheiKanagu <kanagu@kingu.dev>
2023-12-20 10:04:16 +0900

    fix

--- packages/listen_to_music_by_location/lib/my_app.dart
+++ packages/listen_to_music_by_location/lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:core/core.dart';
+import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,24 +13,26 @@ class MyApp extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      builder: (context, child) => MediaQueryPreview(context, child)
-          // .disableDynamicIsland()
-          // .textScale05()
-          .textScale10()
-          // .textScale15()
-          // .textScale20()
-          // .darkMode()
-          // .android()
-          .build(),
-      scaffoldMessengerKey: rootScaffoldMessengerKey,
-      supportedLocales: AppLocaleUtils.supportedLocales,
-      localizationsDelegates: const [
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      routerConfig: ref.watch(myGoRouterProvider),
+    return BetterFeedback(
+      child: MaterialApp.router(
+        builder: (context, child) => MediaQueryPreview(context, child)
+            // .disableDynamicIsland()
+            // .textScale05()
+            .textScale10()
+            // .textScale15()
+            // .textScale20()
+            // .darkMode()
+            // .android()
+            .build(),
+        scaffoldMessengerKey: rootScaffoldMessengerKey,
+        supportedLocales: AppLocaleUtils.supportedLocales,
+        localizationsDelegates: const [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        routerConfig: ref.watch(myGoRouterProvider),
+      ),
     );
   }
 }
