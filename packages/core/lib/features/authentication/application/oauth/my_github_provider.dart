2a014526698963c6dcb19cbcdecda5af9dd37ba3
KoheiKanagu <kanagu@kingu.dev>
2023-12-25 22:09:42 +0900

    fix

--- packages/core/lib/features/authentication/application/oauth/my_github_provider.dart
+++
@@ -1,4 +0,0 @@
-// ignore_for_file: depend_on_referenced_packages, implementation_imports
-import 'package:core/features/authentication/application/oauth/github_provider.dart';
-
-class MyGitHubProvider extends GitHubProvider {}
--- packages/core/lib/features/authentication/application/oauth/oauth_providers.dart
+++ packages/core/lib/features/authentication/application/oauth/oauth_providers.dart
@@ -1,17 +1,17 @@
 // ignore_for_file: depend_on_referenced_packages, implementation_imports
 
-import 'package:core/features/authentication/application/oauth/my_apple_provider.dart';
-import 'package:core/features/authentication/application/oauth/my_github_provider.dart';
-import 'package:core/features/authentication/application/oauth/my_google_provider.dart';
+import 'package:core/features/authentication/application/oauth/github_provider.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 
 final firebaseUIAuthProviders = [
-  MyAppleProvider(),
-  MyGoogleProvider(
+  AppleProvider(),
+  GoogleProvider(
     clientId: 'ignore',
     iOSPreferPlist: true,
     scopes: [
       'email',
     ],
   ),
-  MyGitHubProvider(),
+  GitHubProvider(),
 ];
