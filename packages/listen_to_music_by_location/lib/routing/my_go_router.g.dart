13a34adb726b0bbfa6faab9a19389500531fedbd
KoheiKanagu <kanagu@kingu.dev>
2023-12-26 17:57:14 +0900

    fix

--- packages/listen_to_music_by_location/lib/routing/my_go_router.g.dart
+++ packages/listen_to_music_by_location/lib/routing/my_go_router.g.dart
@@ -8,7 +8,7 @@ part of 'my_go_router.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$myGoRouterHash() => r'2d263707796ed5c6db0871c1ad7bfc8160c081ff';
+String _$myGoRouterHash() => r'cff474b27e6834665410ac96d915633a05514d6e';
 
 /// See also [myGoRouter].
 @ProviderFor(myGoRouter)
