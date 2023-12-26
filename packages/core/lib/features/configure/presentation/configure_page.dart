d07d4c60b3d880e1575f194c15a068a3d600263b
KoheiKanagu <kanagu@kingu.dev>
2023-12-26 15:17:50 +0900

    fix

--- packages/core/lib/features/configure/presentation/configure_page.dart
+++ packages/core/lib/features/configure/presentation/configure_page.dart
@@ -1,6 +1,7 @@
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:collection/collection.dart';
 import 'package:core/core.dart';
+import 'package:core/features/onboarding/application/onboarding_route.dart';
 import 'package:core/i18n/strings.g.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -48,40 +49,7 @@ class ConfigurePage extends HookConsumerWidget {
                     leadingIcon: e.leadingIcon,
                   ),
                 ),
-          if (kDebugMode) ...[
-            ConfigureListTile(
-              title: '[debug] SignOut',
-              onTap: () async {
-                final result = await showOkCancelAlertDialog(
-                  context: context,
-                  title: 'SignOut?',
-                );
-
-                if (result == OkCancelResult.ok) {
-                  await ref.read(firebaseAuthProvider).signOut();
-                  logger.d('SignOut');
-                }
-              },
-              leadingIcon: Icons.logout,
-              trailingIcon: Icons.warning_rounded,
-            ),
-            ConfigureListTile(
-              title: '[debug] clear SharedPreferences',
-              onTap: () async {
-                final result = await showOkCancelAlertDialog(
-                  context: context,
-                  title: 'clear SharedPreferences?',
-                );
-
-                if (result == OkCancelResult.ok) {
-                  await ref.read(sharedPreferencesClearProvider.future);
-                  logger.d('clear SharedPreferences');
-                }
-              },
-              leadingIcon: Icons.clear_all,
-              trailingIcon: Icons.warning_rounded,
-            ),
-          ],
+          const _DebugListTiles(),
           const Divider(),
           ConfigureListTile(
             title: i18n.configure.feedback,
@@ -115,6 +83,62 @@ class ConfigurePage extends HookConsumerWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _DebugListTiles extends HookConsumerWidget {
+  const _DebugListTiles();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    if (!kDebugMode) {
+      return const SizedBox.shrink();
+    }
+
+    return Column(
+      children: [
+        ConfigureListTile(
+          title: '[debug] SignOut',
+          onTap: () async {
+            final result = await showOkCancelAlertDialog(
+              context: context,
+              title: 'SignOut?',
+            );
+
+            if (result == OkCancelResult.ok) {
+              await ref.read(firebaseAuthProvider).signOut();
+              logger.d('SignOut');
+            }
+          },
+          leadingIcon: Icons.logout,
+          trailingIcon: Icons.warning_rounded,
+        ),
+        ConfigureListTile(
+          title: '[debug] clear SharedPreferences',
+          onTap: () async {
+            final result = await showOkCancelAlertDialog(
+              context: context,
+              title: 'clear SharedPreferences?',
+            );
+
+            if (result == OkCancelResult.ok) {
+              await ref.read(sharedPreferencesClearProvider.future);
+              logger.d('clear SharedPreferences');
+            }
+          },
+          leadingIcon: Icons.clear_all,
+          trailingIcon: Icons.warning_rounded,
+        ),
+        ConfigureListTile(
+          title: '[debug] go ${const OnboardingPageRoute().location}',
+          onTap: () {
+            const OnboardingPageRoute().go(context);
+          },
+          leadingIcon: Icons.start_rounded,
+          trailingIcon: Icons.warning_rounded,
+        ),
+      ],
     );
   }
 }
