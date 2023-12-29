c7889b706d88b4055ae24353b63fe0835da3c85a
KoheiKanagu <kanagu@kingu.dev>
2023-12-29 14:38:52 +0900

    fix

---
+++ packages/listen_to_music_by_location/lib/features/music/application/geomusic_providers.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: require_trailing_commas, non_constant_identifier_names, cast_nullable_to_non_nullable, lines_longer_than_80_chars, unnecessary_null_checks, strict_raw_type
+
+part of 'geomusic_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$geomusicCollectionReferenceHash() =>
+    r'981799902c5ef19657426fb831d1094990d740b6';
+
+/// See also [geomusicCollectionReference].
+@ProviderFor(geomusicCollectionReference)
+final geomusicCollectionReferenceProvider =
+    AutoDisposeProvider<CollectionReference<Geomusic>>.internal(
+  geomusicCollectionReference,
+  name: r'geomusicCollectionReferenceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$geomusicCollectionReferenceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GeomusicCollectionReferenceRef
+    = AutoDisposeProviderRef<CollectionReference<Geomusic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
