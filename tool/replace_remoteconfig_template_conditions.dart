bc93779ad68713a757487be486c9edc6bf017a14
KoheiKanagu <kanagu@kingu.dev>
2024-02-04 22:04:28 +0900

    fix

--- tool/replace_remoteconfig_template_conditions.dart
+++ tool/replace_remoteconfig_template_conditions.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_dynamic_calls, lines_longer_than_80_chars
+// ignore_for_file: lines_longer_than_80_chars
 
 import 'dart:convert';
 import 'dart:io';
@@ -9,58 +9,68 @@ import 'package:grinder/grinder.dart';
   "Replace remoteconfig.template.json's conditions",
 )
 void replaceRemoteconfigTemplateConditions() {
+  final list = run(
+    'firebase',
+    arguments: [
+      'apps:list',
+      '--json',
+    ],
+  );
+  // e.g.
+  // {
+  //   "status": "success",
+  //   "result": [
+  //     {
+  //       "name": "projects/kingu-garage-dev/iosApps/1:682848709439:ios:3361e1e37cb0d30884c18a",
+  //       "displayName": "listen_to_music_by_location",
+  //       "platform": "IOS",
+  //       "appId": "1:682848709439:ios:3361e1e37cb0d30884c18a",
+  //       "namespace": "dev.kingu.listenToMusicByLocation.dev",
+  //       "apiKeyId": "f76e615c-c000-4b6e-8aff-798a7246d7be",
+  //       "state": "ACTIVE",
+  //       "expireTime": "1970-01-01T00:00:00Z"
+  //     }
+  //   ]
+  // }
+  final response = json.decode(list) as Map<String, dynamic>;
+  final result = response['result'] as List<dynamic>;
+
   final target = File('remoteconfig.template.json');
-  final raw = target.readAsStringSync();
+  final template =
+      json.decode(target.readAsStringSync()) as Map<String, dynamic>;
 
-  final template = json.decode(raw) as Map<String, dynamic>;
-  template['conditions'] = (template['conditions'] as List<dynamic>).map(
-    (e) {
-      // Keep expressions that are not defined for individual apps as they are
-      if (!(e['expression'] as String).startsWith('app.id == ')) {
-        log('Skip: ${e['name']}');
-        return e;
-      }
-      log('Replace: ${e['name']}');
+  // e.g.
+  // "conditions": [
+  //   {
+  //     "name": "dev.kingu.listenToMusicByLocation",
+  //     "expression": "app.id == '1:682848709439:ios:3361e1e37cb0d30884c18a'",
+  //     "tagColor": "ORANGE"
+  //   }
+  // ],
+  final conditions = template['conditions'] as List<dynamic>;
 
-      // suffixes are '_ios' or '_android'
-      final name = (e['name'] as String)
-          .replaceFirst('_ios', '')
-          .replaceFirst('_android', '');
-      final expression = e['expression'] as String;
-
-      final String platform;
-      if (expression.contains('ios')) {
-        platform = 'ios';
-      } else if (expression.contains('android')) {
-        platform = 'android';
-      } else {
-        throw Exception('Unknown platform');
-      }
-
-      final firebaseJson = json.decode(
-        // e.g. packages/listen_to_music_by_location/firebase.json
-        File('packages/$name/firebase.json').readAsStringSync(),
-      ) as Map<String, dynamic>;
-
-      // Verify the appId for dev
-      final devAppId = firebaseJson['flutter']['platforms']['dart']
-              ['lib/constants/firebase_options_dev.dart']['configurations']
-          [platform] as String;
-      log('devAppId: $devAppId');
-      if (expression != "app.id == '$devAppId'") {
-        throw Exception('devAppId does not match');
-      }
-
-      // Get the appId for prod
-      final prodAppId = firebaseJson['flutter']['platforms']['dart']
-              ['lib/constants/firebase_options_prod.dart']['configurations']
-          [platform] as String;
-      log('prodAppId: $prodAppId');
-
-      // Replace the expression
-      return e..['expression'] = "app.id == '$prodAppId'";
-    },
-  ).toList();
+  template['conditions'] = result
+      .map((e) => e as Map<String, dynamic>)
+      .map(
+        (e) => conditions
+            .map((e) => e as Map<String, dynamic>)
+            .firstWhere(
+              (element) =>
+                  element['name'] ==
+                  // e.g.
+                  // dev.kingu.listenToMusicByLocation.dev -> dev.kingu.listenToMusicByLocation
+                  (e['namespace'] as String).replaceFirst(
+                    RegExp(r'\.dev$'),
+                    '',
+                  ),
+            )
+            .map(
+              (key, value) => key == 'expression'
+                  ? MapEntry(key, "app.id == '${e['appId']}'")
+                  : MapEntry(key, value),
+            ),
+      )
+      .toList();
 
   target.writeAsStringSync(
     const JsonEncoder.withIndent('  ').convert(template),
@@ -73,8 +83,4 @@ void replaceRemoteconfigTemplateConditions() {
       'diff',
     ],
   );
-}
-
-void main(List<String> args) {
-  replaceRemoteconfigTemplateConditions();
 }
