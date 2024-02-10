af7d5b1798596db74c5776ebf932091358b24a70
KoheiKanagu <kanagu@kingu.dev>
2024-02-10 19:04:33 +0900

    refactor(common): use FLUTTER_APP_FLAVOR

--- packages/listen_to_music_by_location/lib/main_dev.dart
+++ packages/listen_to_music_by_location/lib/main_dev.dart
@@ -4,7 +4,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:listen_to_music_by_location/my_app.dart';
 
 Future<void> main() async {
-  appEnv = AppEnv.dev;
   kTenantId = 'loca-music-l7m49';
   kAppStoreId = '6471416156';
 
--- packages/listen_to_music_by_location/lib/main_prod.dart
+++ packages/listen_to_music_by_location/lib/main_prod.dart
@@ -4,7 +4,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:listen_to_music_by_location/my_app.dart';
 
 Future<void> main() async {
-  appEnv = AppEnv.prod;
   kTenantId = 'loca-music-58i3e';
   kAppStoreId = '6471416156';
 
