d9824fe0836e33dbafd008a8018112a34dc07363
KoheiKanagu <kanagu@kingu.dev>
2024-02-10 19:14:05 +0900

    refactor(common): use FLUTTER_APP_FLAVOR (#91)

--- packages/core/lib/constants/app_env.dart
+++ packages/core/lib/constants/app_env.dart
@@ -13,7 +13,14 @@ String get kDatabaseId {
 /// App Store id. Used for [InAppReview]
 late final String kAppStoreId;
 
-late final AppEnv appEnv;
+/// [appFlavor constant \- services library \- Dart API](https://api.flutter.dev/flutter/services/appFlavor-constant.html)
+const _appFlavor = String.fromEnvironment('FLUTTER_APP_FLAVOR');
+
+final appEnv = switch (_appFlavor) {
+  'dev' => AppEnv.dev,
+  'prod' => AppEnv.prod,
+  _ => throw Exception('Invalid AppEnv: $_appFlavor'),
+};
 
 final kAppEnvDev = appEnv == AppEnv.dev;
 final kAppEnvProd = appEnv == AppEnv.prod;
--- packages/listen_to_music_by_location/test/main_screenshot.dart
+++ packages/listen_to_music_by_location/test/main_screenshot.dart
@@ -13,7 +13,6 @@ import 'package:listen_to_music_by_location/gen/strings.g.dart';
 import 'package:listen_to_music_by_location/my_app.dart';
 
 Future<void> main() async {
-  appEnv = AppEnv.dev;
   kTenantId = 'loca-music-l7m49';
   kAppStoreId = '6471416156';
 
