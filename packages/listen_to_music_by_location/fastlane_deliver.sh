55bd1dbb7c3fb8be9547d82feb6b7ac196bf5c94
KoheiKanagu <kanagu@kingu.dev>
2024-02-13 21:20:43 +0900

    fix

--- packages/listen_to_music_by_location/fastlane_deliver.sh
+++ packages/listen_to_music_by_location/fastlane_deliver.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
+version=$(cider version)
+
+# 1.0.0+1 → 1.0.0
+app_version=${version%%+*}
+
 fastlane deliver \
     --skip_binary_upload true \
     --skip_screenshots true \
     --precheck_include_in_app_purchases false \
+    --app_version "$app_version" \
     --force true \
     --app_identifier dev.kingu.listenToMusicByLocation
 
