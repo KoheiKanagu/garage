2e4fde9530ada9fde1ad79c22b783f20af09c578
KoheiKanagu <kanagu@kingu.dev>
2024-02-16 20:07:06 +0900

    fix

---
+++ packages/core/test/features/configure/presentation/user_info_page_test.dart
@@ -0,0 +1,15 @@
+void main() {
+  // TODO: OAuthProviderButtonBaseでFirebase.initializeAppが必要になるので実行できない
+  // goldenTestCombo(
+  //   testName: 'UserInfoPage',
+  //   widget: const UserInfoPage(),
+  //   supportedLocales: AppLocaleUtils.supportedLocales,
+  //   cupertinoThemeData: const CupertinoThemeData(),
+  //   materialThemeData: ThemeData(),
+  //   providerOverrides: [
+  //     firebaseUserLinkedProvidersProvider.overrideWith(
+  //       (ref) => [],
+  //     ),
+  //   ],
+  // );
+}
