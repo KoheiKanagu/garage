9b772e161ef8be8a29085d19a6790828f37e0a8c
KoheiKanagu <kanagu@kingu.dev>
2024-02-16 19:51:30 +0900

    fix

---
+++ packages/core/test/features/configure/presentation/update_app_page_test.dart
@@ -0,0 +1,43 @@
+import 'package:core/core.dart';
+import 'package:core/gen/strings.g.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:test/test.dart';
+
+import '../../../core.dart';
+
+void main() {
+  setUpAll(() async {
+    await initBudouX();
+  });
+
+  goldenTestCombo(
+    testName: 'UpdateAppPageHome not forced',
+    widget: const UpdateAppPageHome(
+      force: false,
+      noAppIconImage: true,
+    ),
+    supportedLocales: AppLocaleUtils.supportedLocales,
+    cupertinoThemeData: const CupertinoThemeData(),
+    materialThemeData: ThemeData(),
+    providerOverrides: [
+      packageInfoAppNameProvider.overrideWithValue('App Name'),
+      packageInfoInstallerStoreProvider.overrideWithValue('Store'),
+    ],
+  );
+
+  goldenTestCombo(
+    testName: 'UpdateAppPageHome forced',
+    widget: const UpdateAppPageHome(
+      force: true,
+      noAppIconImage: true,
+    ),
+    supportedLocales: AppLocaleUtils.supportedLocales,
+    cupertinoThemeData: const CupertinoThemeData(),
+    materialThemeData: ThemeData(),
+    providerOverrides: [
+      packageInfoAppNameProvider.overrideWithValue('App Name'),
+      packageInfoInstallerStoreProvider.overrideWithValue('Store'),
+    ],
+  );
+}
