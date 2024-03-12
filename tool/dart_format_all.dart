3fbdeea99d760594767c76381fc79ad8a18134d0
KoheiKanagu <kanagu@kingu.dev>
2024-03-12 22:32:55 +0900

    refactor: melos run dart:fix:formatを高速化

---
+++ tool/dart_format_all.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:grinder/grinder.dart';
+
+import 'utils.dart';
+
+@Task(
+  'Run dart format on all packages',
+)
+Future<void> dartFormatAll() async {
+  final packages = await runMelosList();
+
+  final targets = [
+    Directory('tool'),
+    ...packages.map(
+      (e) => Directory('${e.path}/lib'),
+    ),
+    ...packages.map(
+      (e) => Directory('${e.path}/test'),
+    ),
+  ];
+
+  final files = targets
+      .map(
+        (e) => e
+            .listSync(recursive: true)
+            .where((e) => e.path.endsWith('.dart'))
+            .whereNot((e) => e.path.endsWith('.gen.dart'))
+            .whereNot((e) => e.path.endsWith('.freezed.dart'))
+            .whereNot((e) => e.path.endsWith('.g.dart')),
+      )
+      .flattened;
+
+  run(
+    'dart',
+    arguments: [
+      'format',
+      ...files.map((e) => e.path),
+    ],
+  );
+}
--- tool/grind.dart
+++ tool/grind.dart
@@ -3,6 +3,7 @@ import 'package:grinder/grinder.dart';
 export 'bump.dart';
 export 'bump_all_deps.dart';
 export 'create_new_release.dart';
+export 'dart_format_all.dart';
 export 'deliver_store_metadata.dart';
 export 'dump_firestore_indexes.dart';
 export 'flutterfire_configure.dart';
