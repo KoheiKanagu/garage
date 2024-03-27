046326b69a57a434fc4d77e730e2b9b3d45a9d07
KoheiKanagu <kanagu@kingu.dev>
2024-03-27 10:48:07 +0900

    fix

--- firebase_emulators_test.sh
+++ firebase_emulators_test.sh
@@ -7,6 +7,11 @@ npm --prefix firebase/functions ci
 npm --prefix firebase/functions run clean
 npm --prefix firebase/functions run compile
 
+# @swc/jestを使っているのでjsにコンパイルする必要はない。
+# むしろ、コンパイルすると external-account-credential.ts で指定している相対パスが崩れるので
+# 削除する。
+npm --prefix firebase/functions run clean
+
 firebase emulators:exec \
     --only auth,functions,firestore \
     --import firebase/testdata \
