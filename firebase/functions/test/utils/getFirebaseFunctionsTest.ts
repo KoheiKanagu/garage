579bd284e9ecdb4b055990702652baecce883370
KoheiKanagu <kanagu@kingu.dev>
2024-03-27 14:43:54 +0900

    fix

--- firebase/functions/test/utils/getFirebaseFunctionsTest.ts
+++ firebase/functions/test/utils/getFirebaseFunctionsTest.ts
@@ -2,7 +2,21 @@ import { kDevProjectId } from '../../src/utils/constants';
 
 /* eslint-disable node/no-unpublished-require */
 export function getFirebaseFunctionsTest() {
-  return require('firebase-functions-test')({
-    projectId: kDevProjectId,
-  });
+  // const test = new FirebaseFunctionsTest();
+  // test.init({
+  //   projectId: kDevProjectId,
+  // });
+
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    const ExternalAccountCredential =
+      require('../../scripts/utils/external-account-credential').ExternalAccountCredential;
+    return require('firebase-functions-test')({
+      credential: new ExternalAccountCredential(),
+      projectId: kDevProjectId,
+    });
+  } else {
+    return require('firebase-functions-test')({
+      projectId: kDevProjectId,
+    });
+  }
 }
