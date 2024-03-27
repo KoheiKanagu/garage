92e1bd68088c5078e58e814533217fd7fe59b388
KoheiKanagu <kanagu@kingu.dev>
2024-03-27 15:11:44 +0900

    fix

--- firebase/functions/test/utils/getFirebaseFunctionsTest.ts
+++ firebase/functions/test/utils/getFirebaseFunctionsTest.ts
@@ -2,21 +2,34 @@ import { kDevProjectId } from '../../src/utils/constants';
 
 /* eslint-disable node/no-unpublished-require */
 export function getFirebaseFunctionsTest() {
-  // const test = new FirebaseFunctionsTest();
-  // test.init({
-  //   projectId: kDevProjectId,
-  // });
+  //   const test = new FirebaseFunctionsTest();
+  //   test.init({
+  //     projectId: kDevProjectId,
+  //   });
 
-  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    const ExternalAccountCredential =
-      require('../../scripts/utils/external-account-credential').ExternalAccountCredential;
-    return require('firebase-functions-test')({
-      credential: new ExternalAccountCredential(),
+  return require('firebase-functions-test')(
+    {
       projectId: kDevProjectId,
-    });
-  } else {
-    return require('firebase-functions-test')({
-      projectId: kDevProjectId,
-    });
-  }
+    },
+    process.env.GOOGLE_APPLICATION_CREDENTIALS
+  );
+
+  // if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  //   const ExternalAccountCredential =
+  //     require('../../scripts/utils/external-account-credential').ExternalAccountCredential;
+  //   return require('firebase-functions-test')(
+  //     {
+  //       credential: new ExternalAccountCredential(),
+  //       projectId: kDevProjectId,
+  //     },
+  //     process.env.GOOGLE_APPLICATION_CREDENTIALS
+  //   );
+  // } else {
+  //   return require('firebase-functions-test')(
+  //     {
+  //       projectId: kDevProjectId,
+  //     },
+  //     process.env.GOOGLE_APPLICATION_CREDENTIALS
+  //   );
+  // }
 }
