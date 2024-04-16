2767e987ded7c5bb4bf1cc8e9d371aafb23046c6
KoheiKanagu <kanagu@kingu.dev>
2024-04-16 18:00:15 +0900

    setup

--- packages/core/test/test_utils.dart
+++ packages/core/test/test_utils.dart
@@ -1,5 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:test/test.dart';
+import 'package:patrol/patrol.dart';
+
+export 'package:flutter_test/flutter_test.dart';
+export 'package:patrol/patrol.dart';
 
 ProviderContainer createContainer({
   ProviderContainer? parent,
@@ -15,4 +19,30 @@ ProviderContainer createContainer({
   addTearDown(container.dispose);
 
   return container;
+}
+
+const _nativeAutomatorConfig = NativeAutomatorConfig(
+  findTimeout: Duration(seconds: 20), // 10 seconds is too short for some CIs
+);
+
+// Future<void> createApp(PatrolIntegrationTester $) async {
+//   await app_main.main();
+//   await $.pumpAndSettle();
+// }
+
+void patrol(
+  String description,
+  Future<void> Function(PatrolIntegrationTester) callback, {
+  bool? skip,
+  NativeAutomatorConfig? nativeAutomatorConfig,
+  LiveTestWidgetsFlutterBindingFramePolicy framePolicy =
+      LiveTestWidgetsFlutterBindingFramePolicy.fadePointers,
+}) {
+  patrolTest(
+    description,
+    nativeAutomatorConfig: nativeAutomatorConfig ?? _nativeAutomatorConfig,
+    framePolicy: framePolicy,
+    skip: skip,
+    callback,
+  );
 }
