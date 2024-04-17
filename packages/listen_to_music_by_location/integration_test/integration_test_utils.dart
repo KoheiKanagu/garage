fead4e4a0ea9a06868dc2efb45c39fd525facef9
KoheiKanagu <kanagu@kingu.dev>
2024-04-17 22:03:19 +0900

    fix

--- packages/listen_to_music_by_location/integration_test/integration_test_utils.dart
+++ packages/listen_to_music_by_location/integration_test/integration_test_utils.dart
@@ -2,6 +2,6 @@ import 'package:listen_to_music_by_location/main.dart' as app_main;
 import 'package:patrol/patrol.dart';
 
 Future<void> createApp(PatrolIntegrationTester $) async {
-  app_main.main();
+  await app_main.main();
   await $.pumpAndSettle();
 }
