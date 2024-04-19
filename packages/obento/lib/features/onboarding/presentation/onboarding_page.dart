aa077ddb006e4675ba3eb6c5acb1cde3578b1a15
KoheiKanagu <kanagu@kingu.dev>
2024-04-19 23:46:38 +0900

    onboarding

--- packages/obento/lib/features/onboarding/presentation/onboarding_page.dart
+++ packages/obento/lib/features/onboarding/presentation/onboarding_page.dart
@@ -2,6 +2,7 @@ import 'package:core/core.dart' hide Assets;
 import 'package:core/gen/strings.g.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:obento/gen/assets.gen.dart';
 
 class OnboardingPage extends HookConsumerWidget {
   const OnboardingPage({
@@ -12,8 +13,16 @@ class OnboardingPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return OnboardingPageBase(
       children: switch (LocaleSettings.currentLocale) {
-        AppLocale.ja => [],
-        AppLocale.en => []
+        AppLocale.ja => [
+            Assets.images.appIcon.image(),
+            Assets.images.appIcon.image(),
+            Assets.images.appIcon.image(),
+          ],
+        AppLocale.en => [
+            Assets.images.appIcon.image(),
+            Assets.images.appIcon.image(),
+            Assets.images.appIcon.image(),
+          ]
       },
     );
   }
