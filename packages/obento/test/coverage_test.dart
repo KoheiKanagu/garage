712077ecb4ab0662c49c7c6d2e772b9a5222d9d6
KoheiKanagu <kanagu@kingu.dev>
2024-04-26 12:22:08 +0900

    fix

---
+++ packages/obento/test/coverage_test.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: unused_import
+import 'package:obento/features/home/application/home_route.dart';
+import 'package:obento/features/home/presentation/home_page.dart';
+import 'package:obento/features/onboarding/application/onboarding_route.dart';
+import 'package:obento/features/onboarding/presentation/onboarding_page.dart';
+
+void main() {}
