d302384d3284a67a6fe6914877df31bd9c80e136
KoheiKanagu <kanagu@kingu.dev>
2024-05-10 22:22:16 +0900

    Update translations for error_tag_already_exists string in Japanese and English

--- packages/obento/lib/gen/strings.g.dart
+++ packages/obento/lib/gen/strings.g.dart
@@ -139,6 +139,7 @@ class Translations implements BaseTranslations<AppLocale, Translations> {
 	String get app_name => '#お弁当記録';
 	String get create_new => '新しい項目を作成';
 	String get tamagoyaki => '卵焼き';
+	String get error_tag_already_exists => 'この項目は既に作成されています';
 }
 
 // Path: <root>
@@ -164,4 +165,5 @@ class _StringsEn extends Translations {
 	@override String get app_name => '#obento';
 	@override String get create_new => 'Create New';
 	@override String get tamagoyaki => 'Tamagoyaki';
+	@override String get error_tag_already_exists => 'This item already exists';
 }
