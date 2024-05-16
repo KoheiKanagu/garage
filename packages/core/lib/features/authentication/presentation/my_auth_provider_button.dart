b303cbf6064c7eb3c59398d201de4ab311e980ed
KoheiKanagu <kanagu@kingu.dev>
2024-05-16 10:23:59 +0900

    fix: Sign In with button style (#322)

--- packages/core/lib/features/authentication/presentation/my_auth_provider_button.dart
+++ packages/core/lib/features/authentication/presentation/my_auth_provider_button.dart
@@ -29,128 +29,132 @@ class MyAuthProviderButton extends HookConsumerWidget {
 
     final isProgress = useState(false);
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: type.logoBackgroundColorOf(context),
-      ),
-      height: _logoSize,
-      width: double.infinity,
-      child: ref.watch(myAuthProviderIsLinkedProvider(type)).maybeWhen(
-            orElse: () => Center(
-              child: CircularProgressIndicator.adaptive(
-                backgroundColor: type.textColorOf(context),
-              ),
-            ),
-            data: (isLinked) => GestureDetector(
-              onTap: () async {
-                if (isProgress.value) {
-                  return;
-                }
-                final indicator = showMyProgressIndicator(
-                  builder: (context) => const SizedBox.shrink(),
-                );
+    final isLinked = ref.watch(myAuthProviderIsLinkedProvider(type));
 
-                try {
-                  isProgress.value = true;
-
-                  if (isLinked) {
-                    final result = await showOkCancelAlertDialog(
-                      context: context,
-                      title: i18n.auth.unlink_confirm(
-                        account: type.providerName,
+    final child = isLinked.maybeWhen(
+      orElse: () => indicatorWidget(context),
+      data: (isLinked) => AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        child: isProgress.value
+            ? indicatorWidget(context)
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: _logoSize,
+                    height: _logoSize,
+                    padding: type.logoPadding,
+                    child: ScalableImageWidget.fromSISource(
+                      si: ScalableImageSource.fromSI(
+                        DefaultAssetBundle.of(context),
+                        type.logoAssetPathOf(context),
                       ),
-                      isDestructiveAction: true,
-                      okLabel: i18n.auth.unlink,
-                    );
-
-                    if (result == OkCancelResult.ok) {
-                      await authProviderController.unlink();
-                    }
-                  } else {
-                    await authProviderController.signInOrLink();
-                  }
-                } on Exception catch (e, stack) {
-                  if (e is CredentialAlreadyInUseException) {
-                    final providerName = e.type.providerName;
-
-                    if (context.mounted) {
-                      await showOkAlertDialog(
-                        context: context,
-                        title: i18n.auth.credential_already_in_use_exception,
-                        message: i18n.auth
-                            .credential_already_in_use_exception_message(
-                          providerName: providerName,
-                        ),
-                      );
-                      return;
-                    }
-                  }
-
-                  logger.handle(e, stack);
-
-                  if (context.mounted) {
-                    await showOkAlertDialog(
-                      context: context,
-                      title: i18n.auth.exception,
-                      message: i18n.auth.exception_message,
-                    );
-                  }
-                } finally {
-                  isProgress.value = false;
-                  indicator.dismiss();
-                }
-              },
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 300),
-                child: isProgress.value
-                    ? const Center(
-                        child: CircularProgressIndicator.adaptive(
-                          backgroundColor: Colors.white,
-                        ),
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: _logoSize,
-                            height: _logoSize,
-                            padding: type.logoPadding,
-                            child: ScalableImageWidget.fromSISource(
-                              si: ScalableImageSource.fromSI(
-                                DefaultAssetBundle.of(context),
-                                type.logoAssetPathOf(context),
-                              ),
+                    ),
+                  ),
+                  Text(
+                    isLinked ? type.unlinkLabel : type.signInLabel,
+                    style: switch (themeType) {
+                      InheritedThemeType.material =>
+                        Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w600,
+                              color: type.textColorOf(context),
                             ),
-                          ),
-                          Text(
-                            isLinked ? type.unlinkLabel : type.signInLabel,
-                            style: switch (themeType) {
-                              InheritedThemeType.material => Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(
-                                    fontSize: 19,
-                                    fontWeight: FontWeight.w600,
-                                    color: type.textColorOf(context),
-                                  ),
-                              InheritedThemeType.cupertino =>
-                                CupertinoTheme.of(context)
-                                    .textTheme
-                                    .textStyle
-                                    .copyWith(
-                                      fontSize: 19,
-                                      fontWeight: FontWeight.w600,
-                                      color: type.textColorOf(context),
-                                    ),
-                            },
-                            textScaler: TextScaler.noScaling,
-                          ),
-                        ],
-                      ),
+                      InheritedThemeType.cupertino =>
+                        CupertinoTheme.of(context).textTheme.textStyle.copyWith(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w600,
+                              color: type.textColorOf(context),
+                            ),
+                    },
+                    textScaler: TextScaler.noScaling,
+                  ),
+                ],
               ),
+      ),
+    );
+
+    Future<void> onPressed() async {
+      if (isProgress.value) {
+        return;
+      }
+      final indicator = showMyProgressIndicator(
+        builder: (context) => const SizedBox.shrink(),
+      );
+
+      try {
+        isProgress.value = true;
+
+        if (isLinked.value ?? false) {
+          final result = await showOkCancelAlertDialog(
+            context: context,
+            title: i18n.auth.unlink_confirm(
+              account: type.providerName,
             ),
+            isDestructiveAction: true,
+            okLabel: i18n.auth.unlink,
+          );
+
+          if (result == OkCancelResult.ok) {
+            await authProviderController.unlink();
+          }
+        } else {
+          await authProviderController.signInOrLink();
+        }
+      } on Exception catch (e, stack) {
+        if (e is CredentialAlreadyInUseException) {
+          final providerName = e.type.providerName;
+
+          if (context.mounted) {
+            await showOkAlertDialog(
+              context: context,
+              title: i18n.auth.credential_already_in_use_exception,
+              message: i18n.auth.credential_already_in_use_exception_message(
+                providerName: providerName,
+              ),
+            );
+            return;
+          }
+        }
+
+        logger.handle(e, stack);
+
+        if (context.mounted) {
+          await showOkAlertDialog(
+            context: context,
+            title: i18n.auth.exception,
+            message: i18n.auth.exception_message,
+          );
+        }
+      } finally {
+        isProgress.value = false;
+        indicator.dismiss();
+      }
+    }
+
+    return switch (themeType) {
+      InheritedThemeType.material => FilledButton(
+          onPressed: onPressed,
+          style: FilledButton.styleFrom(
+            backgroundColor: type.logoBackgroundColorOf(context),
+            foregroundColor: type.textColorOf(context),
           ),
+          child: child,
+        ),
+      InheritedThemeType.cupertino => CupertinoButton(
+          onPressed: onPressed,
+          padding: EdgeInsets.zero,
+          color: type.logoBackgroundColorOf(context),
+          child: child,
+        ),
+    };
+  }
+
+  Widget indicatorWidget(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator.adaptive(
+        backgroundColor: type.textColorOf(context),
+      ),
     );
   }
 }
