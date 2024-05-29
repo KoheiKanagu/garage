4a419a55b5d12a94f4c6c4a1b50765aa5ab9ef8d
KoheiKanagu <kanagu@kingu.dev>
2024-05-29 20:30:35 +0900

    fix

--- .github/workflows/flutter_bootstrap.sh
+++ .github/workflows/flutter_bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+echo "MELOS_SDK_PATH=$FLUTTER_ROOT" >>"$GITHUB_ENV"
+
 dart pub global activate melos
 dart pub global activate grinder
 dart pub global activate flutterfire_cli
