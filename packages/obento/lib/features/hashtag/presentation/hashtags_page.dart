b79a89f9564974e57a48e2dd9b3482cc5c24f07e
KoheiKanagu <kanagu@kingu.dev>
2024-06-01 17:49:02 +0900

    feat: push preview page

--- packages/obento/lib/features/hashtag/presentation/hashtags_page.dart
+++ packages/obento/lib/features/hashtag/presentation/hashtags_page.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-
-import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:core/core.dart';
 import 'package:core/gen/strings.g.dart' as core_i18n;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:obento/features/hashtag/application/hashtag_providers.dart';
@@ -13,6 +9,7 @@ import 'package:obento/features/hashtag/presentation/hashtags_page_body.dart';
 import 'package:obento/features/hashtag/presentation/hashtags_page_cancel_button.dart';
 import 'package:obento/features/hashtag/presentation/hashtags_page_menu_button.dart';
 import 'package:obento/features/hashtag/presentation/hashtags_page_save_button.dart';
+import 'package:obento/features/preview/application/preview_route.dart';
 import 'package:obento/gen/strings.g.dart';
 
 class HashtagsPage extends HookConsumerWidget {
@@ -86,54 +83,16 @@ class _FloatingActionButton extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Future<void> onPreview() async {
-      final value = await ref.watch(hashtagPreviewProvider.future);
-      await HapticFeedback.mediumImpact();
-
-      if (context.mounted) {
-        if (value.isEmpty) {
-          await showOkAlertDialog(
-            context: context,
-            title: i18n.not_selected,
-          );
-          return;
-        }
-
-        final result = await showOkCancelAlertDialog(
-          context: context,
-          okLabel: core_i18n.i18n.copy,
-          cancelLabel: core_i18n.i18n.close,
-          message: value,
-        );
-
-        if (result == OkCancelResult.ok) {
-          await HapticFeedback.heavyImpact();
-          await Clipboard.setData(
-            ClipboardData(
-              text: value,
-            ),
-          );
-
-          if (context.mounted) {
-            await showOkAlertDialog(
-              context: context,
-              title: core_i18n.i18n.copied_to_clipboard,
-            );
-          }
-        }
-      }
-    }
-
     final themeType = InheritedThemeDetector.of(context);
     return switch (themeType) {
       InheritedThemeType.material => FloatingActionButton.extended(
           heroTag: 'preview_button',
-          onPressed: onPreview,
+          onPressed: () => const PreviewPageRoute().push<void>(context),
           label: Text(i18n.preview),
         ),
       InheritedThemeType.cupertino => CupertinoButton.filled(
           padding: const EdgeInsets.all(16),
-          onPressed: onPreview,
+          onPressed: () => const PreviewPageRoute().push<void>(context),
           child: Text(
             i18n.preview,
             style: CupertinoTheme.of(context).textTheme.textStyle.copyWith(
