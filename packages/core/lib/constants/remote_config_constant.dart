bdf8cbfd8b3ec8487a3735886df4739f8b158891
KoheiKanagu <kanagu@kingu.dev>
2024-06-02 13:03:45 +0900

    feat: Update terms of service and privacy policy URLs (#373)

--- packages/core/lib/constants/remote_config_constant.dart
+++ packages/core/lib/constants/remote_config_constant.dart
@@ -8,9 +8,9 @@ class RemoteConfigConstant {
 
   static const String kTermsOfServiceUri = 'terms_of_service_uri';
   static const String kTermsOfServiceUriDefaultValue =
-      'https://github.com/KoheiKanagu/garage/blob/main/term_of_service.md';
+      'https://garage.kingu.dev/terms-of-service';
 
   static const String kPrivacyPolicyUri = 'privacy_policy_uri';
   static const String kPrivacyPolicyUriDefaultValue =
-      'https://github.com/KoheiKanagu/garage/blob/main/privacy_policy.md';
+      'https://garage.kingu.dev/privacy-policy';
 }
